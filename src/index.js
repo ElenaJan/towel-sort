3ace89d92b4d7b96f161dd939f0b97b8b3abb22f
ElenaJan <janehek.elena@gmail.com>
2023-01-25 01:09:00 +0300

    fix: delete check

--- src/index.js
+++ src/index.js
@@ -18,29 +18,3 @@ module.exports = function towelSort (matrix) {
   }
   return result;
 }
-
-// function towelSort (matrix) {
-//   let newMatrix = [];
-//   for(let i = 0; i < matrix.length; i++) {
-//     if(i === 0 || i % 2 === 0) {
-//       newMatrix.push(matrix[i]);
-//     } else {
-//       let elem = matrix[i].reverse();
-//       newMatrix.push(elem);
-//     }
-//   }
-//   let arr = [];
-//   for(let i = 0; i < newMatrix.length; i++) {
-//     arr = arr.concat(newMatrix[i]);
-//   }
-//   let result = arr.map((item) => Number(item))
-//   return result;
-// }
-
-// const matrix = [
-//   [1, 2, 4],
-//   [5, 6, 7, 8],
-//   [9, 12],
-// ];
-
-// towelSort(matrix);
